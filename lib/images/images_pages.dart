05f5952b006aa3628080781e29113c0ec3d1690d
Bruno Roberto <brunominasgeraisbrn@gmail.com>
2026-02-18 08:11:54 -0300

    SALVAR MAO NA MASSA

---
+++ lib/images/images_pages.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ImagesPages extends StatelessWidget {
+  const ImagesPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Images')));
+  }
+}
