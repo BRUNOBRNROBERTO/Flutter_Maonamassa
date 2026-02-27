770f138402fd527b1d91f3c239a5ae7e82a71233
Bruno Roberto <brunominasgeraisbrn@gmail.com>
2026-02-27 07:32:29 -0300

    final aula passagem parametro

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_maonamassa/navegacao/home_page.dart' as navegacao;
+import 'package:flutter_maonamassa/navegacao_param/detalhe.dart';
+import 'package:flutter_maonamassa/navegacao_param/lista.dart';
 import 'package:flutter_maonamassa/navegacao/page1.dart';
 import 'package:flutter_maonamassa/navegacao/page2.dart';
 import 'package:flutter_maonamassa/navegacao/page3.dart';
@@ -20,12 +22,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       // home: navegacao.HomePage(),
+      initialRoute: '/navegacao_param',
       routes: {
         '/': (_) => navegacao.HomePage(),
         '/page1': (_) => Page1(),
         Page2.routeName: (_) => Page2(),
         '/page3': (_) => Page3(),
         '/page4': (_) => Page4(),
+        '/navegacao_param': (_) => Lista(),
+        '/detalhe': (_) => Detalhe(),
       },
     );
   }
